d5963fcbfa27138c30ca66ee7e8b3b08aabe2b88
Henio Junior <henioajunior@gmail.com>
2024-01-09 12:36:39 -0300

    ParOuImpar Job

---
+++ parOuImpar/src/main/java/com/udemy/springbatch/job/ParImparBatchConfig.java
@@ -0,0 +1,38 @@
+package com.udemy.springbatch.job;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.batch.item.support.IteratorItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@EnableBatchProcessing
+@Configuration
+public class ParImparBatchConfig {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Bean
+    public Job imprimeParImparJob(Step imprimeParImparStep) {
+        return jobBuilderFactory.get("imprimeParImparJob").start(imprimeParImparStep)
+                .incrementer(new RunIdIncrementer()).build();
+    }
+}
