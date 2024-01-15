8aeae09adb66d302931fcea5a436b9c84749de93
Henio Junior <henioajunior@gmail.com>
2024-01-15 18:23:28 -0300

    Leitura de arquivo de múltiplos formatos

---
+++ arquivoMultiplosFormatosJob/src/main/java/com/springbatch/arquivomultiplosformatos/reader/ClienteTransacaoLineMapperConfig.java
@@ -0,0 +1,62 @@
+package com.springbatch.arquivomultiplosformatos.reader;
+
+import com.springbatch.arquivomultiplosformatos.dominio.Cliente;
+import com.springbatch.arquivomultiplosformatos.dominio.Transacao;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.mapping.PatternMatchingCompositeLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.transform.LineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings({"rawtypes", "unchecked"})
+@Configuration
+public class ClienteTransacaoLineMapperConfig {
+
+    @Bean
+    public PatternMatchingCompositeLineMapper lineMapper() {
+        PatternMatchingCompositeLineMapper lineMapper = new PatternMatchingCompositeLineMapper<>();
+        lineMapper.setTokenizers(getTokenizers());//pegam a linha e divide em palavras
+        lineMapper.setFieldSetMappers(getStringFieldSetMapperMap());//mapeia as palavras para um objeto de dominio
+        return lineMapper;
+    }
+
+    private Map<String, LineTokenizer> getTokenizers() {
+        Map<String, LineTokenizer> tokenizers = new HashMap<>();
+        tokenizers.put("0*", clienteLineTokenizer());
+        tokenizers.put("1*", transacaoLineTokenizer());
+        return tokenizers;
+    }
+
+    private LineTokenizer clienteLineTokenizer() {
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setNames("nome", "sobrenome", "idade", "email");
+        lineTokenizer.setIncludedFields(1, 2, 3, 4);
+        return lineTokenizer;
+    }
+
+    private LineTokenizer transacaoLineTokenizer() {
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setNames("id", "descricao", "valor");
+        lineTokenizer.setIncludedFields(1, 2, 3);
+        return lineTokenizer;
+    }
+
+
+    private Map<String, FieldSetMapper> getStringFieldSetMapperMap() {
+        Map<String, FieldSetMapper> fieldSetMappers = new HashMap<>();
+        fieldSetMappers.put("0*", getFieldSetMapper(Cliente.class));
+        fieldSetMappers.put("1*", getFieldSetMapper(Transacao.class));
+        return fieldSetMappers;
+    }
+
+    private static FieldSetMapper getFieldSetMapper(Class classe) {
+        BeanWrapperFieldSetMapper fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(classe);
+        return fieldSetMapper;
+    }
+}
